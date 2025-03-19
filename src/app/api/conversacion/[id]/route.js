6016fb1885b9345a96b5dd1c0f8c67c208e3f5ef
YomiraSC <a20213223@pucp.edu.pe>
2025-03-19 15:49:50 -0500

    firestore conexion

--- src/app/api/conversacion/[id]/route.js
+++ src/app/api/conversacion/[id]/route.js
@@ -1,4 +1,4 @@
-import prisma from "@/lib/prisma";
+/* import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 import { MongoClient } from "mongodb";
 //require('dotenv').config();
@@ -55,4 +55,79 @@ export async function GET(request, context) {
         { status: 500 }
       );
     }
+  } */
+
+  import { NextResponse } from "next/server";
+  import admin from "firebase-admin";
+  import { getToken } from "next-auth/jwt";
+  import prisma from "@/lib/prisma";
+    
+  // Inicializar Firestore solo si no está inicializado
+  if (!admin.apps.length) {
+    const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),  
+    });
   }
+    
+  const db = admin.firestore();
+
+  export async function GET(request, context) {
+    try {
+      const params = await context.params;
+      const { id } = params;//se obtiene de clientesService
+  
+      // Buscar cliente en Prisma (MySQL/PostgreSQL)
+      const cliente = await prisma.cliente.findUnique({
+        where: { cliente_id: parseInt(id) },
+        select: {
+          cliente_id: true,
+          nombre: true,
+          apellido: true,
+          celular: true,
+        },
+      });
+  
+      if (!cliente) {
+        return NextResponse.json({ message: "Cliente no encontrado" }, { status: 404 });
+      }
+  
+      // Consultar Firestore: Obtener mensajes del cliente con id_bot = codigopago
+      const mensajesRef = db.collection("test")
+        .where("celular", "==", cliente.celular)
+        .where("id_bot", "==", "codigopago")  
+        .orderBy("fecha", "asc");
+
+      const mensajesSnap = await mensajesRef.get();
+      console.log("Cantidad de documentos encontrados:", mensajesSnap.size); 
+
+      if (mensajesSnap.empty) {
+        console.log("No se encontraron documentos en Firestore para este cliente.");
+      }
+      
+      const mensajes = mensajesSnap.docs.map(doc => {
+        console.log("ID del documento:", doc.id); // Ver los IDs en la consola
+        return {
+          id: doc.id,
+          ...doc.data(),
+        };
+      });
+      return NextResponse.json({
+        cliente: {
+          nombreCompleto: `${cliente.nombre} ${cliente.apellido}`,
+          celular: cliente.celular,
+        },
+        conversaciones: mensajes,
+      });
+    } catch (error) {
+      console.error("Error al obtener conversaciones del cliente:", error);
+      return NextResponse.json(
+        { message: "Error interno del servidor al obtener las conversaciones" },
+        { status: 500 }
+      );
+    }
+  }
+
+
+
+  
