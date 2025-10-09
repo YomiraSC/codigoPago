ea9fe6b324b3b239e10100b68c876ed17750eddc
Gian2560 <gianlukagb2004@gmail.com>
2025-10-09 15:42:19 -0500

    enviso_ip_privada

--- services/campaignService.js
+++ services/campaignService.js
@@ -13,7 +13,7 @@ import axiosInstance from "./api";
 // };
 
 const API_URL = "/campaigns";
-const CLOUD_RUN_URL = "https://envios-meta-service-codpago-763512810578.us-central1.run.app";
+const CLOUD_RUN_URL = "https://envios-meta-service-codpago-763512810578.us-west1.run.app";
 export const getCampaigns = async (page = 1, pageSize = 10) => {
     const response = await axiosInstance.get(`/campaigns?page=${page}&pageSize=${pageSize}`);
     return response.data;
